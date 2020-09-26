830e49ea6f17c47a9cda3ae026272f1febb170d4
lunacircle4 <lunacircle4@gmail.com>
2020-09-26 18:39:32 +0900

    feat: use bfs

---
+++ Our Codes/etc/협곡에 숨은 티모 찾기/freshtuna/main.py
@@ -0,0 +1,62 @@
+class Solution:
+    def __init__(self, *args, **kwargs):
+        self.m = None
+        self.n = None
+        self.data = None
+        self.query = "teemo" 
+
+        for key, value in kwargs.values():
+            setattr(self, key, value)
+        self.answer = [for y in [for i in self.data]]
+
+    def call(self):
+        self.left_trace()
+        self.right_trace()
+        self.top_trace()
+        self.down_trace()
+        self.right_top_trace()
+        self.right_down_trace()
+        self.left_top_trace()
+        self.left_down_trace()
+
+    def left_trace(self):
+        for i in range(m):
+            for j in range(n):
+                pass
+
+    def right_trace(self):
+        out = 0
+        point
+        for i in range(m):
+            for j in range(n):
+                if self.data[m][n]
+
+    def top_trace(self):
+        for i in range(m):
+            for j in range(n):
+                slef. 
+
+    def down_trace(self):
+        for i in range(m):
+            for j in range(n):
+                slef. 
+
+    def right_top_trace(self):
+        for i in range(m):
+            for j in range(n):
+                slef. 
+
+    def right_down_trace(self):
+        for i in range(m):
+            for j in range(n):
+                slef. 
+
+    def left_top_trace(self):
+        for i in range(m):
+            for j in range(n):
+                slef. 
+
+    def left_down_trace(self):
+        for i in range(m):
+            for j in range(n):
+                slef. 
