2f7c82ee690b55b6d02987de510468a6a002f21c
WooKyungjin <wkj1103@naver.com>
2020-10-25 00:21:31 +0900

    leetcode/valid palindrome, kyungjin

---
+++ Our Codes/Leetcode/[125] valid palindrome/Kyungjin/Solution.java
@@ -0,0 +1,15 @@
+package leetcode_125;
+
+public class Solution {
+
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "").trim();
+
+        for(int i=0; i<s.length()/2; i++) {
+            if(s.charAt(i) != s.charAt(s.length()-1-i))
+                return false;
+        }
+
+        return true;
+    }
+}
