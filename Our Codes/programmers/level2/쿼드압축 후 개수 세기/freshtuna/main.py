35d6d7b3b6fdc987ed0ad7091d133b10a52bb885
lunacircle4 <lunacircle4@gmail.com>
2020-11-19 16:48:39 +0900

    feat: 프로그래머스/level2/jadenCase 문자열 만들기/freshtuna

--- Our Codes/programmers/level2/쿼드압축 후 개수 세기/freshtuna/main.py
+++ Our Codes/programmers/level2/쿼드압축 후 개수 세기/freshtuna/main.py
@@ -1,24 +1,20 @@
 def sub_solution(arr, length, center_x, center_y):
+    result = [0, 0]
+
     if length == 2:
         cases = [arr[center_x][center_y], 
             arr[center_x - 1][center_y], arr[center_x][center_y - 1],
-            arr[center_x - 1][center_y - 1]]
-        
-        zero_case, one_case = 0, 0 
-        for case in cases:
-            if case == 0: zero_case += 1
-            else: one_case += 1
-        
-        if zero_case == 0: return [0, 1]
-        elif one_case == 0: return [1, 0]
-        else: return [zero_case, one_case]
+            arr[center_x - 1][center_y - 1]] 
+        for case in cases: result[case] += 1
+        if result[0] == 0: return [0, 1]
+        elif result[1] == 0: return [1, 0]
+        else: return result
     else:
         cases = [[center_x - length // 4, center_y - length // 4], 
          [center_x - length // 4, center_y + length // 4], 
          [center_x + length // 4, center_y - length // 4], 
          [center_x + length // 4, center_y + length // 4]]
 
-        result = [0, 0]
         for case in cases:
             case_result = sub_solution(arr, length // 2, *case)
             result[0] += case_result[0]
---
+++ Our Codes/programmers/level2/쿼드압축 후 개수 세기/readme.md
@@ -0,0 +1,1 @@
+# [[프로그래머스] [level2] 쿼드압축 후 개수 세기](https://programmers.co.kr/learn/courses/30/lessons/68936)
